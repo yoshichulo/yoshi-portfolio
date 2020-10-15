cb41c8ecb71c6061ed3bf6be7e4d3d276bb13a90
Yoshi <joseangelyoshi@gmail.com>
2020-10-15 22:11:32 +0200

    Added basic files and packets && established part of the layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialbarComponent } from './components/socialbar/socialbar.component';
+import { LanguagesComponent } from './components/languages/languages.component';
+import { InfoPanelComponent } from './components/info-panel/info-panel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SocialbarComponent,
+    LanguagesComponent,
+    InfoPanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    AngularSvgIconModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
