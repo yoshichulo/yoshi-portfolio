83aef5a95a7a58721fd08438165a074312570244
Yoshi <joseangelyoshi@gmail.com>
2020-10-23 11:54:32 +0200

    Added particles.js as background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'yoshi-portfolio';
+
+  constructor(){}
+  ngOnInit(){
+    particlesJS.load('particles-js', 'assets/particles.json', () => {
+      console.log('callback - particles.js config loaded');
+    });
+  }
 }
